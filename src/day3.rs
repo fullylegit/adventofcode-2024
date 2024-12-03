b835e61487727d3393a34390ced22080afd17e4d
user <40304161+fullylegit@users.noreply.github.com>
2024-12-03 20:18:55 +1100

    Day 3 part 2

--- src/day3.rs
+++ src/day3.rs
@@ -7,6 +7,7 @@ use nom::IResult;
 pub fn main() {
     const INPUT: &str = include_str!("../inputs/3");
     println!("day 3 part 1: {}", uncorrupted_mul_sum(INPUT));
+    println!("day 3 part 2: {}", uncorrupted_conditional_mul_sum(INPUT));
 }
 
 fn uncorrupted_mul_sum(input: &str) -> usize {
@@ -20,6 +21,39 @@ fn uncorrupted_mul_sum(input: &str) -> usize {
                 remaining = Some(input);
             }
             Err(_) => remaining = input.get(1..),
+        }
+    }
+
+    total
+}
+
+fn uncorrupted_conditional_mul_sum(input: &str) -> usize {
+    let mut total = 0;
+
+    let mut mul_state = State::Enabled;
+    let mut remaining = Some(input);
+    while let Some(input) = remaining {
+        match mul_state {
+            State::Enabled => {
+                if let Ok((input, (num1, num2))) = mul(input) {
+                    remaining = Some(input);
+                    total += num1 * num2;
+                    continue;
+                }
+                if let Ok((input, ())) = disable(input) {
+                    remaining = Some(input);
+                    mul_state = State::Disabled;
+                    continue;
+                }
+                remaining = input.get(1..);
+            }
+            State::Disabled => match enable(input) {
+                Ok((input, ())) => {
+                    remaining = Some(input);
+                    mul_state = State::Enabled;
+                }
+                Err(_) => remaining = input.get(1..),
+            },
         }
     }
 
@@ -42,16 +76,41 @@ fn number(input: &str) -> IResult<&str, usize> {
     })(input)
 }
 
+fn enable(input: &str) -> IResult<&str, ()> {
+    let (remaining, _) = tag("do()")(input)?;
+    Ok((remaining, ()))
+}
+
+fn disable(input: &str) -> IResult<&str, ()> {
+    let (remaining, _) = tag("don't()")(input)?;
+    Ok((remaining, ()))
+}
+
+enum State {
+    Enabled,
+    Disabled,
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
 
-    const INPUT: &str = "xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))";
+    const INPUT1: &str = "xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))";
+    const INPUT2: &str =
+        "xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))";
 
     #[test]
     fn part_1() {
         let expected = 161;
-        let actual = uncorrupted_mul_sum(INPUT);
+        let actual = uncorrupted_mul_sum(INPUT1);
+
+        assert_eq!(expected, actual);
+    }
+
+    #[test]
+    fn part_2() {
+        let expected = 48;
+        let actual = uncorrupted_conditional_mul_sum(INPUT2);
 
         assert_eq!(expected, actual);
     }
