5dd58ea65ee610b10f16ab3d479ab22a230c462a
user <40304161+fullylegit@users.noreply.github.com>
2024-12-07 18:39:12 +1100

    added a cli flag to execute the slow tasks. defaults to off

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use clap::Parser;
 mod day1;
 mod day2;
 mod day3;
@@ -6,12 +7,22 @@ mod day5;
 mod day6;
 mod day7;
 
+#[derive(Debug, Parser)]
+struct Options {
+    #[arg(short, long)]
+    slow: bool,
+}
+
 fn main() {
+    let opts = Options::parse();
+
     day1::main();
     day2::main();
     day3::main();
     day4::main();
     day5::main();
-    day6::main();
-    day7::main();
+    if opts.slow {
+        day6::main();
+        day7::main();
+    }
 }
